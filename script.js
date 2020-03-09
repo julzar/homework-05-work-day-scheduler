b56a685e34653183b187928c00ba515e311ffda9
julzar <56980945+julzar@users.noreply.github.com>
2020-03-09 15:50:32 -0500

    continued updating js, added more comments

--- script.js
+++ script.js
@@ -1,70 +1,102 @@
 $(document).ready(function() {
 
-    // let currentDay = `<p>${moment().format("dddd, MMMM Do")}</p>`
-
     let day = {
-        currentDay: `<p>${moment().format("dddd, MMMM Do")}</p>`,
         dayStart: 9,
         dayEnd: 18,
-    }
+    };
 
-    let hour = day.dayStart
+    let hour = day.dayStart;
 
+    let hours = [];
     
+    let currentTime = moment().hour();
+
+    let eventObj = {}
+
+    let scheduledEvent = JSON.parse(localStorage.getItem("savedEvents"));
 
 
-    $("#currentDay").html(day.currentDay)
+    // Sets #currentDay html to the current day in "day, month date" format
+    $("#currentDay").html(`<p>${moment().format("dddd, MMMM Do")}</p>`)
 
-    
+    // Checks local storage for saved events. Returns an empty string for null and undefined values, else returns the event.
+    function getEventValue() {
 
-    function renderTimeblock() {
-        for (i = day.dayStart; i < day.dayEnd; i++) {
-            let timeblock = `
-            <div class="row p-2">
-                <div id="hour-col" class="col-1">
-                    <span>${moment(hour, ["HH"]).format("h A")}</span>
-                </div>
-                <div class="col">
-                    <input id="eventInput" class="form-control form-control-lg" type="text" placeholder="Add an Event">
-                </div>
-                <div class="col-1">
-                    <button type="button" class="btn btn-outline-primary btn-lg">Save</button>
-                </div>
-            </div>
-        `
-            $(".container").append(timeblock)
-            hour++
+        if (scheduledEvent == null || scheduledEvent[`eventKey-${hour}`] == undefined) {
+            return "";
+        }
+        else {
+            return scheduledEvent[`eventKey-${hour}`];
         }
     }
-    renderTimeblock()
 
+    // Generates hour values for hours[] based on the values of dayStart and dayEnd. Calls getEventValue()
+    function generateHours() {
+        
+        for (i = day.dayStart; i < day.dayEnd; i++) {
+            hours.push({
+                time: hour,
+                event: getEventValue()
+            });
+            hour++;
+        }
+    }
 
+    generateHours();
 
-    //console.log(moment("17", ["HH"]).format("h A"))
+    // Renders a row for each hour with values from hours[]. Calls setTimeClass()
+    function renderTimeBlocks(hoursData) {
 
-    //console.log(timeblock.dayStart)
+        for (i = 0; i < hoursData.length; i++) {
+            let timeblock = 
+            `
+                <div class="row p-2 ${setTimeClass(hoursData[i].time)}">
+                    <div id="hour-col" class="col-1">
+                        <span>${moment(hoursData[i].time, ["HH"]).format("h A")}</span>
+                    </div>
+                    <div class="col">
+                        <input id="${`input-${hoursData[i].time}`}" class="form-control form-control-lg" type="text" placeholder="No Event Yet" value="${hoursData[i].event}">
+                    </div>
+                    <div class="col-1">
+                        <button type="button" id="${hoursData[i].time}" class="btn btn-outline-primary btn-lg">Save</button>
+                    </div>
+                </div>
+            `
+            $(".container").append(timeblock)
+        }
+    }
 
+    renderTimeBlocks(hours);
+
+    // !BROKEN! this is supposed to set class to past, present, or future based on relationship to current time
+    function setTimeClass(validationTime) {
+        //let timeTense = moment([validationTime, "hours"])
+        //let timeDiff = validationTime.diff(currentTime)
+        let timeDiff = moment().diff(validationTime, currentTime);
+        
+        console.log(timeDiff)
+        if(timeDiff > 0) {
+            return "future";
+        } else if( timeDiff == 0) {
+            return "present"
+        }
+        return "past";
+    }
+
+    // Adds onclick events to each "save" button
+    $("button").on('click', function(ev) {
+
+        let eventId = ev.target.id
+        let eventInput = {
+            [`eventKey-${eventId}`]: $(`#input-${eventId}`).val()
+        }
+        // Updates eventObj{} with values from input elements and local storage.
+        $.extend(eventObj, scheduledEvent) // Prevents existing events from being overwritten with empty strings
+        $.extend(eventObj, eventInput)
+        // Updates local storage item "savedEvents" with eventObj{} values
+        localStorage.setItem("savedEvents", JSON.stringify(eventObj))
+    })
+    
 
 // End of script   
 })
-
-
-
-    // var timeblock = {
-    //     blockHTML: `
-    //         <div class="row p-2">
-    //             <div id="hour-col" class="col-1">
-    //                 <span>${moment(hour, ["HH"]).format("h A")}</span>
-    //             </div>
-    //             <div class="col">
-    //                 <input id="eventInput" class="form-control form-control-lg" type="text" placeholder="Add an Event">
-    //             </div>
-    //             <div class="col-1">
-    //                 <button type="button" class="btn btn-outline-primary btn-lg">Save</button>
-    //             </div>
-    //         </div>
-    //     `,
-    //      render() {
-    //          $(".container").append(this.blockHTML) 
-    //      }      
-    // }   
